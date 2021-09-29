3f57dd9b61b58f29b22bb51dfeace8560d925275
PolkaNew <89734031+PolkaNew@users.noreply.github.com>
2021-09-29 12:57:33 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
+money = int(input("Введите сумму депозита:"))
+per_cent_values = list(per_cent.values())
+profit = [round((i * money) / 100, 2) for i in per_cent_values]
+max_profit = max(profit)
+print(profit)
+print("Максимальная сумма, которую вы можете заработать -", round(max_profit, 2))
